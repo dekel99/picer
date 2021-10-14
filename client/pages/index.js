26fd33ca1444bec1100470d0a23981a838a304b5
dekel <dekel99s@gmail.com>
2021-10-14 13:39:50 +0300

    loading screen for server sleep

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,9 +2,10 @@ import { useEffect, useState } from 'react'
 import Redirect from "../components/Redirect"
 import styles from "../styles/index.module.css"
 import { CheckAuth } from '../components/CheckAuth'
+import LoadingSmall from '../components/LoadingSmall'
 
 const img = "https://images.unsplash.com/photo-1452587925148-ce544e77e70d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzd8fHBob3RvZ3JhcGh5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
-const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+const text = "With picer you can easily find which one from two pictures is the best for your specific need! just upload two images and decide on which criterion the community should base their votes."
 
 export default function Main() {
   const [isAuth, setIsAuth] = useState()
@@ -28,6 +29,6 @@ export default function Main() {
   } else if (error){
     return <p>{error}</p>
   } else { 
-    return null
+    return <div style={{marginTop: "50px"}}>Waiting for server, please refresh in few seconds <LoadingSmall loading={true}/></div>
   }
 }
